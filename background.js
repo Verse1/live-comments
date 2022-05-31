ba74459aa481de64f84d9259593b73c76683e49c
Verse <verseow@gmail.com>
2022-05-31 04:08:17 -0400

    added back end front end communication

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete' && tab.active) {
+        chrome.tabs.executeScript(tabId, {
+            file: "content.js"
+        });
+    }
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.message == "GET") {
+        
+    }
+});
