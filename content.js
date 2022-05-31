c8e6d0f44e951470064026f161a97af7df5a6946
Verse <verseow@gmail.com>
2022-05-31 06:10:48 -0400

    implemented stack for comment streaming

--- content.js
+++ content.js
@@ -5,6 +5,20 @@ chrome.runtime.sendMessage({
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.comments) {
-    console.log(request.comments);
+    let comments = request.comments.sort((a, b) => {
+      return b.timeStamp - a.timeStamp;
+    });
+
+    setInterval(() => {
+      comments.forEach((comment) => {
+        videoTime = Math.floor(
+          document.querySelector('.video-stream').currentTime
+        );
+        if (comment.timeStamp == videoTime) {
+          console.log(comment);
+          comments.pop();
+        }
+      });
+    }, 100);
   }
 });
