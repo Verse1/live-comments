7265b9298ae287d9564346c1cdca41b85a78b928
Verse <verseow@gmail.com>
2022-06-18 19:04:48 -0400

    added check if tab is a video

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status == 'complete' && tab.active) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (
+    changeInfo.status == 'complete' &&
+    tab.active &&
+    /^https:\/\/www.youtube.com\/watch\*/.test(tab.url)
+  ) {
     chrome.tabs.executeScript(tabId, {
       file: 'content.js',
     });
