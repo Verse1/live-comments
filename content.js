45d76e3feb86a2188c7d5ecf1128cc5b778c6ded
Verse <verseow@gmail.com>
2022-06-18 19:38:18 -0400

    added comment injector

--- content.js
+++ content.js
@@ -3,6 +3,22 @@ chrome.runtime.sendMessage({
   id: new URLSearchParams(location.search).get('v'),
 });
 
+const commentInjector = (commentText) => {
+
+  const vid = document.querySelector('.video-stream').getBoundingClientRect();
+  const comment = document.createElement('div');
+
+  comment.innerText = commentText;
+  comment.style.position = 'absolute';
+  comment.style.top = `${vid.top}px`;
+  comment.style.left = `${vid.left}px`;
+  comment.style.backgroundColor = 'black';
+  comment.style.color = 'white';
+  document.querySelector('body').append(comment);
+}
+
+
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.comments) {
     let comments = request.comments.sort((a, b) => {
@@ -16,9 +32,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         );
         if (comment.timeStamp == videoTime) {
           console.log(comment);
+          commentInjector(comment.text);
           comments.pop();
         }
       });
     }, 100);
   }
 });
+
